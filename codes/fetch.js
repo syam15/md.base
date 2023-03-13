50f4bd9114925adf5db5a6268e6c491af293a6a9
syam15 <33655622+syam15@users.noreply.github.com>
2023-03-13 23:34:21 +0700

    Add files via upload

---
+++ codes/fetch.js
@@ -0,0 +1,13 @@
+"use strict";
+const fetch = require('node-fetch')
+
+exports.fetchJson = (url, options) => new Promise(async(resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
